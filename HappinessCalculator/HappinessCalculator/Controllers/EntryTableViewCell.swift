5c0c7d8ac8dfafe68cdba157b2ae6f4e620efa0b
Kelsey Sparkman <kelseysparkman14@gmail.com>
2020-03-03 12:29:50 -0700

    Added notification

--- HappinessCalculator/HappinessCalculator/Controllers/EntryTableViewCell.swift
+++ HappinessCalculator/HappinessCalculator/Controllers/EntryTableViewCell.swift
@@ -32,12 +32,15 @@ class EntryTableViewCell: UITableViewCell {
         self.entry = entry
         updateUI(averageHappiness: averageHappiness)
     }
-    func updateUI(averageHappiness: Int){
+   @objc func updateUI(averageHappiness: Int){
         guard let entry = entry else {return}
         titleLabel.text = entry.title
         isEnabledSwitch.isOn = entry.isIncluded
         higherOrLowerLabel.text = entry.happiness >= averageHappiness ? "Higher" : "Lower"
-        
+    }
+    
+    func createObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(updateUI), name: notificationKey, object: nil)
     }
     
     @IBAction func toggledIsIncluded(_ sender: Any) {
